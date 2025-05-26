9b9d1416895c2f187b131547eee4399f4ec414ef
Shaik Zhafir <zadus96@gmail.com>
2025-05-26 23:30:39 +0800

    added gook in progress and completed and progress bar update in frontend

---
+++ frontend/src/types/books.types.ts
@@ -0,0 +1,10 @@
+export interface Book {
+  id: number;
+  user_id: number;
+  title: string;
+  author: string;
+  progress: number;
+  isbn: string;
+  description: string;
+  image_url: string;
+}
